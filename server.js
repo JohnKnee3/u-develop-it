de9393513031e3ca4ce5b6d78036a30e94321a7d
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-03 13:31:48 -0700

    12.4 zip squish

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const db = require("./db/connection");
-const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,19 +9,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/api", apiRoutes);
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
 // Start server after DB connection
-db.connect((err) => {
+db.connect(err => {
   if (err) throw err;
-  console.log("Database connected.");
+  console.log('Database connected.');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log("http://localhost:3001/api/candidates");
   });
 });
